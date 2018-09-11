b6f8cb96f9b841df41efd27104c544514886f288
Tom Henricksen <tom.henricksen@gmail.com>
2018-09-11 15:24:54 -0400

    Arrays: Left Rotation
    Check out the resources on the page's right side to learn more about arrays. The video tutorial is by Gayle Laakmann McDowell, author of the best-selling interview book Cracking the Coding Interview.
    
    A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become .
    
    Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
    
    Function Description
    
    Complete the function rotLeft in the editor below. It should return the resulting array of integers.
    
    rotLeft has the following parameter(s):
    
    An array of integers .
    An integer , the number of rotations.
    Input Format
    
    The first line contains two space-separated integers  and , the size of  and the number of left rotations you must perform. 
    The second line contains  space-separated integers .

---
+++ src/com/henricksen/LeftRotationSolution.java
@@ -0,0 +1,68 @@
+package com.henricksen;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LeftRotationSolution {
+
+	// Complete the rotLeft function below.
+	static int[] rotLeft(int[] a, int d) {
+		for (int i = 0, j = a.length - 1; i < j; i++, j--)
+			swap(a, i, j);
+		d %= a.length;
+		if (d > 0) {
+			d = a.length - d;
+			for (int i = 0, j = d - 1; i < j; i++, j--)
+				swap(a, i, j);
+			for (int i = d, j = a.length - 1; i < j; i++, j--)
+				swap(a, i, j);
+		}
+		return a;
+	}
+
+	private static void swap(int[] arr, int i, int j) {
+		int tmp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = tmp;
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		String[] nd = scanner.nextLine().split(" ");
+
+		int n = Integer.parseInt(nd[0]);
+
+		int d = Integer.parseInt(nd[1]);
+
+		int[] a = new int[n];
+
+		String[] aItems = scanner.nextLine().split(" ");
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int i = 0; i < n; i++) {
+			int aItem = Integer.parseInt(aItems[i]);
+			a[i] = aItem;
+		}
+
+		int[] result = rotLeft(a, d);
+
+		for (int i = 0; i < result.length; i++) {
+			bufferedWriter.write(String.valueOf(result[i]));
+
+			if (i != result.length - 1) {
+				bufferedWriter.write(" ");
+			}
+		}
+
+		bufferedWriter.newLine();
+
+		bufferedWriter.close();
+
+		scanner.close();
+	}
+}
